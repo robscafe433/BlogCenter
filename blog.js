189670b1d123896ed4f8271e6c1e8b542f5e3aab
Robert S <robscafe333@yahoo.com>
2024-04-25 12:02:55 -0700

    added the second page html, css and js, with code to pull array of objects from local storage and dynamically create elements and display data from local storage.

---
+++ blog.js
@@ -0,0 +1,30 @@
+let blogArrayFromLS = [];
+const summary = document.getElementById('summary');
+
+blogArrayFromLS = JSON.parse(localStorage.getItem('blogLocalStorage'));
+console.log('here is the data from LS: ', blogArrayFromLS);
+
+//Dynamically creates elements on blog.html
+
+console.log('blogArray typeOf', typeof blogArrayFromLS);
+
+blogArrayFromLS.forEach((element) => {
+    const eachEntryDiv = document.createElement('div');
+    const blogTitle = document.createElement('h3');
+    const blogContent = document.createElement('h3');
+    const blogName = document.createElement('h3');
+
+    summary.append(eachEntryDiv);
+    eachEntryDiv.appendChild(blogTitle);
+    eachEntryDiv.appendChild(blogContent);
+    eachEntryDiv.appendChild(blogName);
+
+    eachEntryDiv.setAttribute('id', 'eachEntryDivId');
+    blogTitle.setAttribute('id', 'blogTitleId');
+    blogContent.setAttribute('id', 'blogContentId');
+    blogName.setAttribute('id', 'blogNameId');
+
+    blogTitle.textContent = element.objName;
+    blogContent.textContent = element.objTitle;
+    blogName.textContent = element.objContent;
+});
